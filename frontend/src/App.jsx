816874fe2f69842950e7ad1acb5022375cf33726
sanjibpaul59 <sanjibpaul171@gmail.com>
2024-02-20 17:13:21 +0600

    Simple containerized full-stack skeleton app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Hola People!</h1>
+        <h3>How is this life gonna end?</h3>
+      </div>
+    </>
+  )
+}
+
+export default App
